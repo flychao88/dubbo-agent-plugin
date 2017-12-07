94f3c460b1456d3d895da8ae6d7f6e4f2a715ad6
flychao88 <chengchao80@yeah.net>
2017-12-07 10:35:54 +0800

    增加注释

--- src/main/java/com/dubboagent/communication/elasticsearch/ESConfig.java
+++ src/main/java/com/dubboagent/communication/elasticsearch/ESConfig.java
@@ -6,9 +6,14 @@ package com.dubboagent.communication.elasticsearch;
  * @author:chao.cheng
  **/
 public class ESConfig {
+    //es IP地址
     public static String ip;
+    //ES 端口号
     public static int port;
+    //ES 索引名称
     public static String indexName;
+
+    //agent包扫描
     public static String packageScanPath;
 
 
